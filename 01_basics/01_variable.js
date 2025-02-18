4fd18d4fb74f9b6bd9de6766e2cb79b6bbf86efc
MYcool8853 <121431176+MYcool8853@users.noreply.github.com>
2025-02-18 20:40:34 +0000

    done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const id = 11
+let email = "mycool@gmail.com"
+var password = 2233;
+city = "varanasi"  // allow declare without using any variable
+let state;
+
+// id = 4 not allow reassign,redeclare
+// let email = "irfan@gmail.com" ----not allow redeclare 
+email = "irfan@gmail.com"  // ----allow reassign
+
+var password = 5566    // ----allow redeclare and reassign
+city = "delhi" 
+
+console.log(id);
+console.table([id,email,password,city,state]);
+console.log(id,email,password,city,state);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let a = null; // null
+let b = 89;   // number range is 2 to power 53
+let c = true; //boolean
+let d = 214687469836487346327469127649182; // BigInt
+let e = "mycool";    //String
+let f = Symbol();    // Symbol()
+let g;               // undenfined
+
+console.log(a,b,c,d,e,f,g);
+console.table([typeof a,typeof b,typeof c,typeof d,typeof e,typeof f,typeof g]);
