d745e215411432b69f7258f6d8d250ab68adcfa6
MYcool8853 <121431176+MYcool8853@users.noreply.github.com>
2025-02-18 15:59:42 +0000

    config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,11 +1,1 @@
-const id = 1234
-let email = "mycool@gmail.com"
-var password = 4455 
-city = "varanasi"
-
-// id = 4 not allowed
-console.log(id);
-
-console.log([id,email,password,city]);
-
-
+console.log("mycool");
