6290f35575d5952950a3ef0ff5ae130b4dd35123
MYcool8853 <121431176+MYcool8853@users.noreply.github.com>
2025-02-18 15:56:59 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("mycool");
+const id = 1234
+let email = "mycool@gmail.com"
+var password = 4455 
+city = "varanasi"
+
+// id = 4 not allowed
+console.log(id);
+
+console.log([id,email,password,city]);
+
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const id = 1234
+let email = "mycool@gmail.com"
+var password = 2233;
+city = "varanasi"
+
+// id = 4 not allowed in const
+email = "irfan@gmail.com"
+password = 5566
+city = "delhi"
+
+console.log(id);
+console.table([id,email,password,city]);
+
