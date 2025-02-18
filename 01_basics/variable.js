e31f67f1c71ae79b79c30cd4402d7456003e40ec
MYcool8853 <121431176+MYcool8853@users.noreply.github.com>
2025-02-18 16:15:27 +0000

    done

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,13 +1,16 @@
 const id = 1234
 let email = "mycool@gmail.com"
 var password = 2233;
-city = "varanasi"
+city = "varanasi"  // allow declare without using any variable
+let state;
 
-// id = 4 not allowed in const
-email = "irfan@gmail.com"
-password = 5566
-city = "delhi"
+// id = 4 not allow reassign,redeclare
+// let email = "irfan@gmail.com" ----not allow redeclare 
+email = "irfan@gmail.com"  // ----allow reassign
+
+var password = 5566    // ----allow redeclare and reassign
+city = "delhi" 
 
 console.log(id);
-console.table([id,email,password,city]);
+console.table([id,email,password,city,state]);
 
