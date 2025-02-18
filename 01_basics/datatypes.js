1b570aab29b050b2b59b9c20a3f521f1b466c656
MYcool8853 <121431176+MYcool8853@users.noreply.github.com>
2025-02-18 19:28:10 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,10 +1,10 @@
-let a = null;
-let b = 89;  // number range is 2 to power 53
-let c = true;
-let d = 214687469836487346327469127649182648912749312647461872351934432468132746891264981;
-let e = "mycool";
-let f = Symbol();
-let g;
+let a = null; // null
+let b = 89;   // number range is 2 to power 53
+let c = true; //boolean
+let d = 214687469836487346327469127649182; // BigInt
+let e = "mycool";    //String
+let f = Symbol();    // Symbol()
+let g;               // undenfined
 
 console.log(a,b,c,d,e,f,g);
 console.table([typeof a,typeof b,typeof c,typeof d,typeof e,typeof f,typeof g]);
