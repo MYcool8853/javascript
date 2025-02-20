2affbb7a3543048c01c2945a4ed05d38f72cd250
MYcool8853 <121431176+MYcool8853@users.noreply.github.com>
2025-02-20 19:15:18 +0000

    done

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,19 @@
+const score = 400
+
+const balance = new Number(100.244)
+console.log(balance);
+
+console.log(balance.toString().length); //toString convert in string and lenght find number of character
+console.log(balance.toFixed(2));   // after the decimal give the 2 value only 
+
+const value = 73.454
+console.log(value.toPrecision(4));  // give the number of precision value
+
+const dollar = 10000
+console.log(dollar.toLocaleString('en-IN'));  // convert into indian number system output->1,000
+
+//-----------------------------------MATHS-------------------------------------
+
+console.log(Math);
+
+
