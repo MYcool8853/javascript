42ea7ac0911d9d519b74b2761dc0bd10cbef4ea1
MYcool8853 <121431176+MYcool8853@users.noreply.github.com>
2025-02-20 20:02:00 +0000

    done

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -15,5 +15,23 @@ console.log(dollar.toLocaleString('en-IN'));  // convert into indian number syst
 //-----------------------------------MATHS-------------------------------------
 
 console.log(Math);
+console.log(Math.abs(-4));       //covert the only (-)minus value into (+)plus
+console.log(Math.round(7.6));    //round off the value
+console.log(Math.ceil(4.02));    // round off the highest value output->5
+console.log(Math.floor(4.9));   // round off the lowest value output->4
+console.log("min:",Math.min(4,9,2.2,6,7,));   // include the decimal number
+console.log("max:",Math.max(4,1,44,2,11,6));
 
+// -------------------important------------------------
+console.log(Math.random());          // give the value between 0 and 1
+
+console.log(Math.random()*10);       // shift the one value before decimal
+
+console.log((Math.random()*10) + 1);   // avoid the (0) condition 0.01224 because in dice 1 to 6 number only
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random()*(max-min + 1)) + min); //give the minimum value 10
 
