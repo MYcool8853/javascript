9694348f53fa5b729115ede8203f0ebf623e8a5c
MYcool8853 <121431176+MYcool8853@users.noreply.github.com>
2025-02-22 10:59:03 +0000

    done

---
+++ 02_basics/03_object.js
@@ -0,0 +1,12 @@
+// we can declare the Object{} in two ways 1."Literals Object" 2."Constructor Object"
+// the concept of singleton--> Literals Object('not create singleton')
+// Constructor Object('create singleton')
+
+// Object literals
+
+const user = {
+    name :"mycool",
+    age:18,
+    city: "varansi",
+    email:"mycool@gmail.com"
+}
